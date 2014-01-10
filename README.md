a71ac1ab43cdd334cb727cc5295b96057f2f598a
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-01-10 22:58:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+IO♪MIDI♪INSTRUMENT
+==================
+
+__Musical instruments for hackers.__
+Now you can create your own musical instrument using HTML5.
+
+It is a Socket.io server that can send MIDI events to DAWs (such as GarageBand).
+
+
+
+Brief Usage
+-----------
+
+Prerequisites: Node.js.
+
+1. Clone this project.
+2. `npm install`.
+3. Point your device to server.
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+
+var connect = require('connect')
+var midi = require('midi')
+
+var app = connect.createServer()
+app.use(connect.static(__dirname + '/static'))
+
+var http = require('http')
+  , server = http.createServer(app)
+
+var io = require('socket.io').listen(server)
+var output = new midi.output()
+output.openVirtualPort('node-midi virtual port')
+
+var port = Number(process.env.PORT) || 9998
+
+server.listen(port)
+console.log("Listening on port", port)
+
+io.sockets.on('connection', function(socket) {
+  socket.on('midi', function(channel, note, volume) {
+    output.sendMessage([144 + channel, note, volume])
+  })
+})
+
+
